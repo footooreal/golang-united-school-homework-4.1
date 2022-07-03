586cf24a885f22ceaa1f24cfb8f850abb9803cbc
footooreal <107985446+footooreal@users.noreply.github.com>
2022-07-03 22:53:23 +0300

    Add files via upload

--- reverse_string.go
+++ reverse_string.go
@@ -2,12 +2,14 @@ package reverse_string
 
 func ReverseString(input string) (output string) {
 
-	x := len(input) - 1
+	toRune := []rune(input)
+	x := len(toRune) - 1
 
 	for x >= 0 {
-		output = output + string(input[x])
+		output = output + string(toRune[x])
 		x--
 	}
+	return output
 
 	return output
 
